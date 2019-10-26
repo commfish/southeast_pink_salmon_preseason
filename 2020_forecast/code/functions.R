7fdc297d3be66206f1af4305f9e6836da3d3ca39
Miller <sara.miller@dfg.alaska.local>
2019-10-25 16:59:41 -0800

    commit

--- 2020_forecast/code/functions.R
+++ 2020_forecast/code/functions.R
@@ -87,6 +87,24 @@ mape_summary <- function (data,lev = NULL, model = NULL) {
   out
 }
 
-
+#normality test
+eda.norm <- function(x, ...)
+{
+  par(mfrow=c(2,2))
+  if(sum(is.na(x)) > 0)
+    warning("NA's were removed before plotting")
+  x <- x[!is.na(x)]
+  hist(x, main = "Histogram and non-\nparametric density estimate", prob = T)
+  iqd <- summary(x)[5] - summary(x)[2]
+  lines(density(x, width = 2 * iqd))
+  boxplot(x, main = "Boxplot", ...)
+  qqnorm(x)
+  qqline(x)
+  plot.ecdf(x, main="Empirical and normal cdf")
+  LIM <- par("usr")
+  y <- seq(LIM[1],LIM[2],length=100)
+  lines(y, pnorm(y, mean(x), sqrt(var(x))))
+  shapiro.test(x)
+}
 
 
